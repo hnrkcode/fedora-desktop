b2d92a7d0bb3e7098d4b5541ab38091a41a96887
Henrik Petersson <44243358+hnrkcode@users.noreply.github.com>
2026-08-18 20:25:37 +0200

    feat: install golang

---
+++ roles/golang/files/golang.zsh
@@ -0,0 +1,5 @@
+# Sourced automatically by oh-my-zsh from $ZSH_CUSTOM/*.zsh.
+if [[ -d /usr/local/go/bin ]]; then
+    # $HOME/go/bin is the default GOBIN, where "go install" drops binaries.
+    export PATH="/usr/local/go/bin:$HOME/go/bin:$PATH"
+fi
